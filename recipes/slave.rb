cd08be3fc5e9b2caf0861db8c0715be53af8ced5
Diego Woitasen <diego@flugel.it>
2016-09-06 21:43:52 -0300

    Install and setup Marathon to use Docker.

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -5,3 +5,15 @@ include_recipe 'marathon'
 include_recipe 'marathon::service'
 
 
+docker_installation 'default' do
+  action :create
+end
+
+file '/etc/mesos-slave/containerizers' do
+  content 'docker,mesos'
+end
+
+file '/etc/mesos-slave/executor_registration_timeout' do
+  content '5mins'
+end
+
