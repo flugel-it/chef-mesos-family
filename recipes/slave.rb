0e30ff015992fa8a9e36d89a57cb20100811d359
Diego Woitasen <diego@flugel.it>
2016-09-08 14:21:55 -0300

    Mesos Slave parameters moved to attributes. Those files in the diff are used only by some packaging that community cookbooks are not using.

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -11,11 +11,3 @@ docker_service_manager 'default' do
   action :start
 end
 
-file '/etc/mesos-slave/containerizers' do
-  content 'docker,mesos'
-end
-
-file '/etc/mesos-slave/executor_registration_timeout' do
-  content '5mins'
-end
-
